6a5e89632b823cbe06a0841b0296c4069a4806e6
Jonathan Steyfkens <jonathan.steyfkens@gmail.com>
2018-01-16 07:44:48 +0000

    Re-adding dedicated ADJUST layer button

--- keyboards/xd75/keymaps/jonathansty/keymap.c
+++ keyboards/xd75/keymaps/jonathansty/keymap.c
@@ -44,7 +44,8 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   GAME,
   LOWER,
-  RAISE
+  RAISE,
+  ADJUST
 };
 
 #define XXXXXXX KC_NO
@@ -78,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { HPR_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_KP_7, KC_KP_8,   KC_KP_9,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
   { CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_KP_4, KC_KP_5,   KC_KP_6,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_KP_1, KC_KP_2,   KC_KP_3,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT },
-  { KC_LCTL, _______,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_KP_0, KC_KP_DOT, KC_NUMLOCK, SFT_BSP, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, _______  },
+  { KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_KP_0, KC_KP_DOT, KC_NUMLOCK, SFT_BSP, RAISE,   KC_RGUI, KC_RALT, KC_RCTL, ADJUST  },
 },
 
 [_GAME] = {
@@ -196,6 +197,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+      case ADJUST:
+        if(record->event.pressed){
+          layer_on(_ADJUST);
+        } else {
+          layer_off(_ADJUST);
+        }
+        return false;
+        break;
   }
   return true;
 }
