330b03b1b79f378ba26ec85cb525fcecde2b9d5c
Jonathan Steyfkens <7695731+jonathansty@users.noreply.github.com>
2025-09-13 07:17:51 +0100

    Update keymap after mege

--- keyboards/ymdk/id75/keymaps/jonathansty/keymap.c
+++ keyboards/ymdk/id75/keymaps/jonathansty/keymap.c
@@ -1,18 +1,5 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// To flash run `qmk flash -kb ymdk/id75/rp2040 -km jonathansty`
+// Your keyboard is using rp2040 most likely
 
 #include QMK_KEYBOARD_H
 #include "action_layer.h"
@@ -110,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { _______, _______, _______, _______, _______, _______, _______,   _______, _______,      _______, _______, PTR_ARROW, KC_PAUSE, KC_INS, KC_PSCR },
   { KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_INSERT, KC_HOME, KC_PAGE_UP,   KC_CIRC, KC_AMPR, KC_ASTR,   KC_LPRN, KC_RPRN, KC_BSPC },
   { KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_DELETE, KC_END,  KC_PAGE_DOWN, KC_F6,   KC_UNDS, KC_PLUS,   KC_LCBR, KC_RCBR, KC_PIPE },
-  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,   _______, _______,      KC_F12,  KC_MS_L, KC_MS_D,   KC_MS_U, KC_MS_R, KC_BTN1 },
+  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,   _______, _______,      KC_F12,  _______, _______,   _______, _______, _______ },
   { _______, _______, _______, _______, _______, _______, _______,   _______, _______,      _______, _______, KC_MNXT,   KC_VOLD, KC_VOLU, KC_MPLY }
 },
 
@@ -131,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
   { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC },
   { KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS },
-  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______, KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BTN2 },
+  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______, KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______ },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY }
 },
 
@@ -149,12 +136,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * `--------------------------------------------------------------------------------------------------------'
  */
+#define RGB_TOG QK_UNDERGLOW_TOGGLE
+#define RGB_MOD QK_UNDERGLOW_MODE_NEXT
+
 [_ADJUST] =  {
-  { _______, _______,       _______, _______, _______, _______, RGB_TOG, RGB_MOD, BL_TOGG, _______, _______, _______, _______, _______, _______ },
-  { _______, QK_BOOT, MAKE_KEYBOARD, _______, _______, _______, RGB_HUI, RGB_HUD, BL_STEP, _______, _______, _______, _______, _______, KC_DEL  },
-  { _______, _______,       _______, _______, _______, _______, RGB_SAI, RGB_SAD, BL_UP,   _______, _______, _______, _______, _______, _______ },
-  { _______, _______,       _______, _______, _______, _______, RGB_VAI, RGB_VAD, BL_DOWN, _______, _______, _______, _______, _______, _______ },
-  { _______, _______,       _______, _______, _______, _______, _______, _______, BL_BRTG, _______, _______, _______, _______, _______, _______ },
+  { _______, _______,       _______, _______, _______, _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, QK_BOOT, MAKE_KEYBOARD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL  },
+  { _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
 },
 
 };
